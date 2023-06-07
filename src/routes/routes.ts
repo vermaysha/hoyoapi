542bd719b3d7685e761a52ef0aa863b595903a6e
Ashary Vermaysha <avermaysha@gmail.com>
2023-06-07 09:53:00 +0700

    feat: add API endpoints to routes file

---
+++ src/routes/routes.ts
@@ -0,0 +1,8 @@
+/* Main API Endpoint */
+export const BBS_API = 'https://bbs-api-os.hoyolab.com'
+export const ACCOUNT_API = 'https://api-account-os.hoyolab.com'
+export const HK4E_API = 'https://sg-hk4e-api.hoyolab.com'
+export const PUBLIC_API = `https://sg-public-api.hoyolab.com`
+
+/* HoYoLab API Endpoint */
+export const USER_GAMES_LIST = `${ACCOUNT_API}/account/binding/api/getUserGameRolesByCookieToken`
