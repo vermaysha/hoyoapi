f620666a9644bbd2361b01d8c680208f3c6cda50
Ashary Vermaysha <avermaysha@gmail.com>
2023-06-08 20:20:05 +0700

    test: add test for constructor with cookie string input

---
+++ tests/hoyolab/hoyolab.test.ts
@@ -0,0 +1,13 @@
+import test from 'ava'
+import { cookie } from './setup'
+import { Cookie, Hoyolab } from '../../src'
+
+test('Hoyolab.constructor() should be able to handle cookie string', async (t) => {
+  const cookieString = Cookie.parseCookie(cookie)
+
+  const gi = new Hoyolab({
+    cookie: cookieString
+  })
+
+  t.deepEqual(gi.cookie, Cookie.parseCookieString(cookieString))
+})
---
+++ tests/hoyolab/setup.ts
@@ -0,0 +1,12 @@
+import {config} from 'dotenv'
+import { ICookie } from '../../src'
+config()
+
+export const cookie: ICookie = {
+  accountId: Number(process.env.ACCOUNT_ID),
+  cookieToken: process.env.COOKIE_TOKEN,
+  ltoken: process.env.LTOKEN ?? '',
+  ltuid: Number(process.env.LTUID),
+  cookieTokenV2: process.env.COOKIE_TOKEN_V2 ?? '',
+  accountIdV2: Number(process.env.ACCOUNT_ID_V2)
+}
