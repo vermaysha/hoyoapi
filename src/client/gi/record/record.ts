be88ce9647e3bc7b74b58a9b7fd1b93f61c454ca
Ashary Vermaysha <avermaysha@gmail.com>
2023-06-09 18:32:46 +0700

    feat: add RecordModule class with methods for interacting with Genshin Impact's record API

---
+++ src/client/gi/record/record.ts
@@ -0,0 +1,194 @@
+import { HoyoAPIError } from '../../../error'
+import { LanguageEnum } from '../../../language'
+import { HTTPRequest } from '../../../request'
+import {
+  GENSHIN_RECORD_AVATAR_BASIC_INFO_API,
+  GENSHIN_RECORD_CHARACTER_API,
+  GENSHIN_RECORD_DAILY_NOTE_API,
+  GENSHIN_RECORD_INDEX_API,
+  GENSHIN_RECORD_SPIRAL_ABYSS_API,
+} from '../../../routes'
+import {
+  IGenshinCharacterSummary,
+  IGenshinCharacters,
+  IGenshinDailyNote,
+  IGenshinRecord,
+  IGenshinSpiralAbyss,
+} from './interfaces'
+import { SpiralAbyssScheduleEnum } from './record.enum'
+
+/**
+ * RecordModule class provides methods to interact with Genshin Impact's record module endpoints.
+ *
+ * @class
+ * @internal
+ * @category Module
+ */
+export class RecordModule {
+  /**
+   * Creates an instance of RecordModule.
+   *
+   * @constructor
+   * @param {HTTPRequest} request - An instance of Request class.
+   * @param {LanguageEnum} lang - The language code to be used in requests.
+   * @param {string | null} region - The server region code in which the user's account resides.
+   * @param {number | null} uid - The user ID of the Genshin Impact account.
+   */
+  constructor(
+    private request: HTTPRequest,
+    private lang: LanguageEnum,
+    private region: string | null,
+    private uid: number | null,
+  ) {}
+
+  /**
+   * Get user's Genshin Impact record
+   *
+   * @async
+   * @function
+   * @returns {Promise<IGenshinRecord>} - User's Genshin Impact record
+   * @throws {HoyoAPIError} If UID parameter is missing or failed to be filled
+   * @remarks
+   * This method sends a request to the Genshin Impact API to get the daily note information for a user.
+   * The user's region and UID must be set before calling this method, otherwise an error will be thrown.
+   */
+  async records(): Promise<IGenshinRecord> {
+    if (!this.region || !this.uid) {
+      throw new HoyoAPIError('UID parameter is missing or failed to be filled')
+    }
+
+    this.request
+      .setQueryParams({
+        server: this.region,
+        role_id: this.uid,
+        lang: this.lang,
+      })
+      .setDs(true)
+
+    const res = (await this.request.send(GENSHIN_RECORD_INDEX_API)).data
+
+    return res as IGenshinRecord
+  }
+
+  /**
+   *
+   * Retrieves the Genshin characters of the user.
+   *
+   * @async
+   * @returns {Promise<IGenshinCharacters>} A Promise that contains the Genshin characters object.
+   * @throws {HoyoAPIError} If UID parameter is missing or failed to be filled.
+   * @remarks
+   * This method sends a request to the Genshin Impact API to get the daily note information for a user.
+   * The user's region and UID must be set before calling this method, otherwise an error will be thrown.
+   */
+  async characters(): Promise<IGenshinCharacters> {
+    if (!this.region || !this.uid) {
+      throw new HoyoAPIError('UID parameter is missing or failed to be filled')
+    }
+
+    this.request
+      .setBody({
+        server: this.region,
+        role_id: this.uid,
+      })
+      .setDs(true)
+
+    const res = (await this.request.send(GENSHIN_RECORD_CHARACTER_API, 'POST'))
+      .data
+
+    return res as IGenshinCharacters
+  }
+
+  /**
+   * Returns the summary information of Genshin Impact game characters.
+   *
+   * @param characterIds - An array of character IDs to retrieve the summary information for.
+   * @returns A Promise that resolves to an object containing the summary information of the characters.
+   * @throws Throws an error if the UID parameter is missing or failed to be filled.
+   * @remarks
+   * This method sends a request to the Genshin Impact API to get the daily note information for a user.
+   * The user's region and UID must be set before calling this method, otherwise an error will be thrown.
+   */
+  async charactersSummary(
+    characterIds: number[],
+  ): Promise<IGenshinCharacterSummary> {
+    if (!this.region || !this.uid) {
+      throw new HoyoAPIError('UID parameter is missing or failed to be filled')
+    }
+
+    this.request
+      .setBody({
+        character_ids: characterIds,
+        role_id: this.uid,
+        server: this.region,
+      })
+      .setDs()
+
+    const res = (
+      await this.request.send(GENSHIN_RECORD_AVATAR_BASIC_INFO_API, 'POST')
+    ).data
+
+    return res as IGenshinCharacterSummary
+  }
+
+  /**
+   * Retrieves information about the player's performance in the Spiral Abyss.
+   *
+   * @param scheduleType - The schedule type of the Abyss, either CURRENT or PREVIOUS.
+   * @returns A Promise that resolves with an object containing the player's Spiral Abyss data.
+   * @throws HoyoAPIError if UID parameter is missing or failed to be filled, or if the given scheduleType parameter is invalid.
+   * @remarks
+   * This method sends a request to the Genshin Impact API to get the daily note information for a user.
+   * The user's region and UID must be set before calling this method, otherwise an error will be thrown.
+   */
+  async spiralAbyss(
+    scheduleType: SpiralAbyssScheduleEnum = SpiralAbyssScheduleEnum.CURRENT,
+  ): Promise<IGenshinSpiralAbyss> {
+    if (!this.region || !this.uid) {
+      throw new HoyoAPIError('UID parameter is missing or failed to be filled')
+    }
+
+    if (
+      Object.values(SpiralAbyssScheduleEnum).includes(scheduleType) === false
+    ) {
+      throw new HoyoAPIError('The given scheduleType parameter is invalid !')
+    }
+
+    this.request
+      .setQueryParams({
+        server: this.region,
+        role_id: this.uid,
+        schedule_type: scheduleType,
+      })
+      .setDs()
+
+    const res = (await this.request.send(GENSHIN_RECORD_SPIRAL_ABYSS_API)).data
+
+    return res as IGenshinSpiralAbyss
+  }
+
+  /**
+   * Retrieve the daily note information for a Genshin Impact user.
+   * @returns Promise<IGenshinDailyNote> The daily note information.
+   * @throws HoyoAPIError if the UID parameter is missing or failed to be filled.
+   * @remarks
+   * This method sends a request to the Genshin Impact API to get the daily note information for a user.
+   * The user's region and UID must be set before calling this method, otherwise an error will be thrown.
+   */
+  async dailyNote(): Promise<IGenshinDailyNote> {
+    if (!this.region || !this.uid) {
+      throw new HoyoAPIError('UID parameter is missing or failed to be filled')
+    }
+
+    this.request
+      .setQueryParams({
+        server: this.region,
+        role_id: this.uid,
+      })
+      .setDs()
+
+    const res = (await this.request.send(GENSHIN_RECORD_DAILY_NOTE_API)).data
+
+    return res as IGenshinDailyNote
+  }
+}
