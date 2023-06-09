002145a0f2793fa825df23afcf0440edc22f2c66
Ashary Vermaysha <avermaysha@gmail.com>
2023-06-09 18:33:52 +0700

    feat: add diary and record modules to genshin impact client

--- src/client/gi/gi.ts
+++ src/client/gi/gi.ts
@@ -7,6 +7,8 @@ import { HTTPRequest } from '../../request'
 import { DEFAULT_REFERER } from '../../routes'
 import { getGenshinRegion } from './gi.helper'
 import { GamesEnum, Hoyolab, IGame } from '../hoyolab'
+import { RecordModule, SpiralAbyssScheduleEnum } from './record'
+import { DiaryEnum, DiaryModule, DiaryMonthEnum } from './diary'
 
 /**
  * The `Genshin` class provides an interface to interact with Genshin Impact-related features on the Mihoyo website.
@@ -27,6 +29,18 @@ export class GenshinImpact {
    *
    */
   readonly redeem: RedeemModule
+
+  /**
+   * The `RecordModule` object provides an interface to interact with the user record feature in Genshin Impact.
+   *
+   */
+  readonly record: RecordModule
+
+  /**
+   * The `DiaryModule` object provides an interface to interact with the user diary feature in Genshin Impact.
+   *
+   */
+  readonly diary: DiaryModule
 
   /**
    * HoyYolab account object
@@ -101,6 +115,13 @@ export class GenshinImpact {
       this.region,
       this.uid,
     )
+    this.record = new RecordModule(
+      this.request,
+      this.lang,
+      this.region,
+      this.uid,
+    )
+    this.diary = new DiaryModule(this.request, this.lang, this.region, this.uid)
   }
 
   /**
@@ -128,5 +149,137 @@ export class GenshinImpact {
     gi.account = game
 
     return gi
+  }
+
+  /**
+   * Get user's Genshin Impact record
+   *
+   * @alias {@link GenshinImpact.record | Genshin.record.records()}
+   * @deprecated Use through {@link GenshinImpact.record | Genshin.record.records()} instead
+   */
+  async records() {
+    return this.record.records()
+  }
+
+  /**
+   * Retrieves the Genshin characters of the user.
+   *
+   * @alias {@link GenshinImpact.record | Genshin.record.characters()}
+   * @deprecated Use through {@link GenshinImpact.record | Genshin.record.characters()} instead
+   */
+  async characters() {
+    return this.record.characters()
+  }
+
+  /**
+   * Returns the summary information of Genshin Impact game characters
+   *
+   * @param characterIds number[] Characters ID
+   * @alias {@link GenshinImpact.record | Genshin.record.charactersSummary()}
+   * @deprecated Use through {@link GenshinImpact.record | Genshin.record.charactersSummary()} instead
+   */
+  async charactersSummary(characterIds: number[]) {
+    return this.record.charactersSummary(characterIds)
+  }
+
+  /**
+   * Retrieves information about the player's performance in the Spiral Abyss.
+   *
+   * @param scheduleType SpiralAbyssScheduleEnum
+   * @alias {@link GenshinImpact.record | Genshin.record.spiralAbyss()}
+   * @deprecated Use through {@link GenshinImpact.record | Genshin.record.spiralAbyss()} instead
+   */
+  async spiralAbyss(
+    scheduleType: SpiralAbyssScheduleEnum = SpiralAbyssScheduleEnum.CURRENT,
+  ) {
+    return this.record.spiralAbyss(scheduleType)
+  }
+
+  /**
+   * Retrieve the daily note information for a Genshin Impact user.
+   *
+   * @alias {@link GenshinImpact.record | Genshin.record.dailyNote()}
+   * @deprecated Use through {@link GenshinImpact.record | Genshin.record.dailyNote()} instead
+   */
+  async dailyNote() {
+    return this.record.dailyNote()
+  }
+
+  /**
+   * Returns the diary information of a given month for a user
+   *
+   * @param month
+   * @alias {@link GenshinImpact.diary | Genshin.diary.diaries()}
+   * @deprecated Use through {@link GenshinImpact.diary | Genshin.diary.diaries()} instead
+   */
+  async diaries(month: DiaryMonthEnum = DiaryMonthEnum.CURRENT) {
+    return this.diary.diaries(month)
+  }
+
+  /**
+   * Returns the diary details of a given type and month for a user
+   *
+   * @param type DiaryEnum
+   * @param month DiaryMonthEnum
+   * @alias {@link GenshinImpact.diary | Genshin.diary.detail()}
+   * @deprecated Use through {@link GenshinImpact.diary | Genshin.diary.detail()} instead
+   */
+  async diaryDetail(
+    type: DiaryEnum,
+    month: DiaryMonthEnum = DiaryMonthEnum.CURRENT,
+  ) {
+    return this.diary.detail(type, month)
+  }
+
+  /**
+   * Retrieves daily information.
+   *
+   * @alias {@link GenshinImpact.daily | Genshin.daily.info()}
+   * @deprecated Use through {@link GenshinImpact.daily | Genshin.daily.info()} instead
+   */
+  dailyInfo() {
+    return this.daily.info()
+  }
+
+  /**
+   * Retrieve daily rewards information.
+   *
+   * @alias {@link GenshinImpact.daily | Genshin.daily.rewards()}
+   * @deprecated Use through {@link GenshinImpact.daily | Genshin.daily.rewards()} instead
+   */
+  dailyRewards() {
+    return this.daily.rewards()
+  }
+
+  /**
+   * Get the daily reward for a specific day or the current day
+   *
+   * @param day number | null
+   * @alias {@link GenshinImpact.daily | Genshin.daily.reward()}
+   * @deprecated Use through {@link GenshinImpact.daily | Genshin.daily.reward()} instead
+   */
+  dailyReward(day: number | null = null) {
+    return this.daily.reward(day)
+  }
+
+  /**
+   * Claim current reward
+   *
+   * @alias {@link GenshinImpact.daily | Genshin.daily.claim()}
+   * @deprecated Use through {@link GenshinImpact.daily | Genshin.daily.claim()} instead
+   */
+  dailyClaim() {
+    return this.daily.claim()
+  }
+
+  /**
+   * Redeems a code for a specific account.
+   *
+   * @param code string
+   * @alias {@link GenshinImpact.daily | Genshin.redeem.claim()}
+   * @deprecated Use through {@link GenshinImpact.daily | Genshin.redeem.claim()} instead
+   */
+  redeemCode(code: string) {
+    return this.redeem.claim(code)
   }
 }
--- src/client/gi/index.ts
+++ src/client/gi/index.ts
@@ -1,3 +1,8 @@
 export * from './gi'
 export * from './gi.helper'
 export * from './gi.interface'
+
+// Export module interfaces
+export * from './diary/diary.interface'
+export * from './record/interfaces'
+export * from './record/record.enum'
