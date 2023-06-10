0f5a825c81081bfc594ed827b68ca2999b574428
Ashary Vermaysha <avermaysha@gmail.com>
2023-06-10 18:22:23 +0700

    docs: update docusaurus config warning level

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'wavyflow', // Usually your GitHub org/user name.
   projectName: 'hoyoapi', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
