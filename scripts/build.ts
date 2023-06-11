e2bc3625a635cd27e46c21d8a3c2d23bd3a62403
Ashary Vermaysha <avermaysha@gmail.com>
2023-06-11 09:07:37 +0700

    build: improve console logs and measure execution time

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,13 @@
 import { execSync } from 'child_process'
 import { buildSync } from 'esbuild'
 import { rmSync, writeFileSync } from 'fs'
+import { performance } from 'perf_hooks'
 import glob from 'tiny-glob'
 ;(async () => {
+  const startTime = performance.now()
+
   // Cleaning dist dir
-  console.log('Cleaning dist directory')
+  console.log('\x1b[32mCleaning dist directory\x1b[0m')
   rmSync('dist', {
     force: true,
     recursive: true,
@@ -14,7 +17,7 @@ import glob from 'tiny-glob'
   const target = ['ESNext', 'node8.17']
   const entryPoints = await glob('./src/**/*.ts')
 
-  console.log('Building dist for node (cjs)...')
+  console.log('\x1b[34mBuilding dist for node (cjs)...\x1b[0m')
   buildSync({
     entryPoints,
     outdir: './dist/cjs',
@@ -29,9 +32,15 @@ import glob from 'tiny-glob'
     flag: 'w',
   })
 
-  console.log('Building dist for node type=module (esm)...')
+  console.log('\x1b[34mBuilding dist for node type=module (esm)...\x1b[0m')
   buildSync({
-    entryPoints: ['./src/index.ts'],
+    entryPoints: [
+      './src/index.ts',
+      './src/client/gi/index.ts',
+      './src/client/hi/index.ts',
+      './src/client/hoyolab/index.ts',
+      './src/client/hsr/index.ts',
+    ],
     outdir: './dist/esm',
     bundle: true,
     sourcemap: false,
@@ -48,8 +57,16 @@ import glob from 'tiny-glob'
     flag: 'w',
   })
 
-  console.log('Generating typescript declaration ...')
+  console.log('\x1b[34mGenerating typescript declaration ...\x1b[0m')
   execSync(
     'tsc --declaration --emitDeclarationOnly --declarationDir ./dist/types/',
+  )
+
+  const endTime = performance.now()
+  const executionTime = (endTime - startTime) / 1000
+  console.log(
+    `\x1b[32mBuild Success with execution time ${executionTime.toFixed(
+      2,
+    )} s\x1b[0m`,
   )
 })()
